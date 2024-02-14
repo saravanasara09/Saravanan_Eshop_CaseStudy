c9951c2bd0b62334b9cc0113bcde44ae911a93dc
saravanasara09 <142519377+saravanasara09@users.noreply.github.com>
2024-02-14 22:16:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import useAuthentication from "./assets/useAuthentication";
-import useServices from "./assets/useServices";
-import { Provider } from "react-redux";
-import store from "./store";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import { thunk } from "redux-thunk";
 
-const ConnectedApp = () => {
-  const { AuthProvider } = useAuthentication();
-  const { ServicesProvider } = useServices();
-  return (
-    <AuthProvider>
-      <ServicesProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ServicesProvider>
-    </AuthProvider>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ConnectedApp />);
+export default createStore(rootReducer, applyMiddleware(thunk));
